fc5c324913b617522c4ba6b9d9186b3cab424d7b
shivam-guptas <Er.shivam.guptas@gmail.com>
2026-03-18 09:26:30 -0600

    remove lightning studio

--- tools/lightning-studio/assets/salesforce-config.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  window.LightningStudioSalesforceConfig = {
-    clientId:
-      "3MVG9rZjd7MXFdLjjpxp09DfArjPTkAw.U2nOk4.MA4BoBppjhRcgviwmxu3_jZeiXgIs_APdy0gxzeADeiSN",
-    redirectUri: "https://pulsagi.com/tools/lightning-studio/",
-    scopes: ["api"]
-  };
-})();
